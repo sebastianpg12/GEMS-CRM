03ed6f281c475a66ccbfead3d2c6d658efe9ff6a
Sebastiï¿½n Pulgarï¿½n <sebastian.pulgarin@s1-inconcertcc.com>
2025-09-07 13:00:29 -0500

    Configure project for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/GEMS-CRM/', // Reemplaza con el nombre exacto de tu repositorio
+  build: {
+    outDir: 'dist',
+    sourcemap: false
+  }
 })
