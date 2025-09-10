466411ea6cc7b5a6c14b4c0f6fed78a46f0e622f
Sebastiï¿½n Pulgarï¿½n <sebastian.pulgarin@s1-inconcertcc.com>
2025-09-10 16:42:37 -0500

    Build: ensure vue3-toastify present in deps; wire plugin in main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,25 @@ import App from './App.vue'
 import router from './router'
 import './style.css'
 import { injectSwalStyles } from './composables/useNotifications'
+import Vue3Toastify, { type ToastContainerOptions } from 'vue3-toastify'
+import 'vue3-toastify/dist/index.css'
 
 const app = createApp(App)
 const pinia = createPinia()
 
 // Inyectar estilos personalizados de SweetAlert2
 injectSwalStyles()
+
+// Configurar Vue3Toastify
+app.use(Vue3Toastify, {
+  autoClose: 3000,
+  position: 'top-right',
+  theme: 'dark',
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+} as ToastContainerOptions)
 
 app.use(pinia)
 app.use(router)
